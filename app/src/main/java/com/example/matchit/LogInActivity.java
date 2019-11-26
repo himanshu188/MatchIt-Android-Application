ff11c80104f67df29a2bf645fb3ba5321c5014ba
Himanshu Patel <ptlhimanshu827@gmail.com>
2019-11-26 17:31:22 -0500

    Login Activity is working

--- app/src/main/java/com/example/matchit/LogInActivity.java
+++ app/src/main/java/com/example/matchit/LogInActivity.java
@@ -2,10 +2,28 @@ package com.example.matchit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class LogInActivity extends AppCompatActivity {
 
@@ -14,14 +32,76 @@ public class LogInActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        final TextView userview = findViewById(R.id.username);
+        final TextView passwordview = findViewById(R.id.password);
+
         Button button = (Button) findViewById(R.id.button_sign_in);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), DashBoardActivity.class);
-                finish();
-                view.getContext().startActivity(intent);
+                String username = userview.getText().toString();
+                String password = passwordview.getText().toString();
+                new LoginTask(LogInActivity.this.getApplicationContext(), LogInActivity.this).execute(username, password);
             }
         });
+    }
+}
+
+class LoginTask extends AsyncTask<String, Integer, Integer> {
+
+    private Context context;
+    private LogInActivity logInActivity;
+
+    protected LoginTask(Context context, LogInActivity logInActivity){
+        this.context = context.getApplicationContext();
+        this.logInActivity = logInActivity;
+    }
+    @Override
+    protected Integer doInBackground(String... strings) {
+        Log.d("Testing","Reached");
+        String line;
+        StringBuffer jsonString = new StringBuffer();
+        Integer responseCode = 0;
+        try {
+            URL url = new URL(Constant.PATH + "/user/get");
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("POST");
+            urlConnection.setRequestProperty("Accept-Charset", "UTF-8");
+            urlConnection.setDoOutput(true);
+            try {
+                String requestBody = "username=" + URLEncoder.encode(strings[0], "UTF-8")  + "&password=" + URLEncoder.encode(strings[1], "UTF-8");
+                OutputStreamWriter writer = new OutputStreamWriter(urlConnection.getOutputStream());
+                writer.write(requestBody);
+                writer.close();
+//                urlConnection.connect();
+                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                BufferedReader br = new BufferedReader(new InputStreamReader(in));
+                while((line = br.readLine()) != null){
+                    jsonString.append(line);
+                }
+                br.close();
+                responseCode = urlConnection.getResponseCode();
+            } finally {
+                urlConnection.disconnect();
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Log.d("Response", responseCode.toString());
+        return responseCode;
+    }
+
+    @Override
+    protected void onPostExecute(Integer integer) {
+        super.onPostExecute(integer);
+        if(integer == HttpsURLConnection.HTTP_OK){
+            Intent intent = new Intent(context, DashBoardActivity.class);
+            context.startActivity(intent);
+            logInActivity.finish();
+        } else {
+            Toast.makeText(context, "Username / Password Error", Toast.LENGTH_SHORT).show();
+        }
     }
 }
